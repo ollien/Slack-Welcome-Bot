c7b57235b669c3fac99bc1380d667fdd71e8ca3c
Nick Krichevsky <njk828@gmail.com>
2016-06-28 18:50:40 -0400

    Add welcome message pinging using real time api

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import os
 
 slackClient = slackclient.SlackClient(os.environ["SLACK_TOKEN"])
 slackClient.rtm_connect()
-
+slackClient.api_call("channels.join", name="#electrical")
 while True:
-    print(slackClient.rtm_read())
+    for message in slackClient.rtm_read():
+        print(message)
+        if message["type"] == "team_join":
+            username = message["user"]["name"]
+            print(username)
+            message = "Welcome to the New Ro-Bots Slack, @{}! Please make sure to download this on your phone so we can get your attention! The app is available on both iOS and Android.".format(username)
+            slackClient.api_call("chat.postMessage", channel="#general",
+                text=message, username="The New Ro-Bot", link_names = True)
     time.sleep(5)
