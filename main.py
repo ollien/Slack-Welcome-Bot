596a88f7a8b2d0157fd365645fc59f3fa0767e7e
Nick Krichevsky <njk828@gmail.com>
2016-06-28 18:54:24 -0400

    Remove Debug Prints

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import os
 slackClient = slackclient.SlackClient(os.environ["SLACK_TOKEN"])
 slackClient.rtm_connect()
 slackClient.api_call("channels.join", name="#electrical")
+lastPingTime = 0
 while True:
     for message in slackClient.rtm_read():
-        print(message)
         if message["type"] == "team_join":
             username = message["user"]["name"]
-            print(username)
             message = "Welcome to the New Ro-Bots Slack, @{}! Please make sure to download this on your phone so we can get your attention! The app is available on both iOS and Android.".format(username)
             slackClient.api_call("chat.postMessage", channel="#general",
                 text=message, username="The New Ro-Bot", link_names = True)
+
+    now = time.time()
+    if now - lastPingTime >= 3:
+        slackClient.server.ping()
+        lastPingTime = now
+
     time.sleep(.1)
