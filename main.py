e1dffa732ab0a2f9468e16c026a392e3d35cdd9c
Nick Krichevsky <njk828@gmail.com>
2016-06-28 18:08:28 -0400

    Add test of slack rtm read

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import slackclient
 import time
 import os
 
+slackClient = slackClient.SLackClient(os.environ["SLACK_TOKEN"])
+slackClient.rtm_connect()
+
+while True:
+    print(slackClient.rtm_read())
+    time.sleep(5)
